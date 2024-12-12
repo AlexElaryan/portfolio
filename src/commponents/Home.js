602272f960611f684c95c4065e7f12508a525bf5
AlexElaryan <alexelaryan1@gmail.com>
2024-12-12 16:45:44 +0400

    Fixed for deployment

--- src/commponents/Home.js
+++ src/commponents/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import { Link } from "react-scroll";
+import hero from "assets/img/me.webp"
 
 export default function Start() {
 
     return (
-        <div id="home" className="hero bg-[url('../public/assets/img/me.webp')] w-full h-screen flex flex-col gap-2 justify-center items-center px-[10px]">
+        <div id="home" style={{backgroundImage: hero,}} className="hero w-full h-screen flex flex-col gap-2 justify-center items-center px-[10px]">
             <h1 className="text-center text-4xl font-bold text-white z-10">I AM ALEX ELARYAN</h1>
             <h2 className="text-center text-lg pb-1 border-b-2 z-10 border-white text-white">I'M A CREATIVE WEB DEVELOPER</h2>
             <h3 className="text-center text-sm font-medium text-white z-10">I STAND ON A SWEET SPOT WHERE DESIGN & CODE INTERSECTS.</h3>
